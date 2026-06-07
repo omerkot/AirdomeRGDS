d5b961e336b27742d765683f2b5cfddd8a1aa410
AirdomeRGDS Maintainer <airdomergds@example.invalid>
2026-06-07 18:50:19 +0300

    Decouple launcher from PortMaster controls

--- ports/AirdomeRGDS.sh
+++ ports/AirdomeRGDS.sh
@@ -2,21 +2,6 @@
 
 export HOME=/root
 
-if [ -d "/mnt/ports/PortMaster/" ]; then
-  controlfolder="/mnt/ports/PortMaster"
-elif [ -d "/opt/system/Tools/PortMaster/" ]; then
-  controlfolder="/opt/system/Tools/PortMaster"
-elif [ -d "/opt/tools/PortMaster/" ]; then
-  controlfolder="/opt/tools/PortMaster"
-else
-  controlfolder="/mnt/mmc/Ports/PortMaster"
-fi
-
-if [ -f "$controlfolder/control.txt" ]; then
-  source "$controlfolder/control.txt"
-  get_controls
-fi
-
 SHDIR=$(dirname "$0")
 GAMEDIR="$SHDIR/AirdomeRGDS"
 
@@ -28,12 +13,14 @@ export XDG_RUNTIME_DIR="/var/run"
 export WAYLAND_DISPLAY="wayland-0"
 export SDL_VIDEODRIVER="wayland"
 export LD_LIBRARY_PATH="/usr/lib:$GAMEDIR/libs:$LD_LIBRARY_PATH"
+unset SDL_GAMECONTROLLERCONFIG
+unset SDL_GAMECONTROLLERCONFIG_FILE
 
 LCDSWAP="$(cat /sys/class/anbernic_misc/lcdswap 2>/dev/null)"
 if [[ "$LCDSWAP" == "1" ]]; then
-  SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig" SDL2_SWAP_LCD=1 ./airdomergds --fullscreen 2>&1 | tee "$GAMEDIR/log.txt"
+  SDL2_SWAP_LCD=1 ./airdomergds --fullscreen 2>&1 | tee "$GAMEDIR/log.txt"
 else
-  SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig" ./airdomergds --fullscreen 2>&1 | tee "$GAMEDIR/log.txt"
+  ./airdomergds --fullscreen 2>&1 | tee "$GAMEDIR/log.txt"
 fi
 
 systemctl restart oga_events >/dev/null 2>&1 &
